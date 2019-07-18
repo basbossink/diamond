2e1f2c2272ac8f3dd9f8f4891dc1031d6b84ff84
Bas Bossink <bas.bossink@divverence.com>
2019-07-18 23:40:45 +0200

    First implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,150 @@
+static UPPERS: &str = "ABCDEFHIJKLMNOPQRSTUVWXYZ";
+
+pub fn diamond(letter: char) -> String {
+    format!("{0}{1}", upper_diamond(letter), lower_diamond(letter))
+}
+
+fn upper_diamond(letter: char) -> String {
+    let letters = letters_upto(letter);
+    let half_width = letters.len();
+    let mut result = String::new();
+    for (index, letter) in letters.iter().enumerate() {
+        let left = format!("{0:>width$}", letter, width = half_width - index);
+        result.push_str(&left);
+        if index != 0 {
+            result.push_str(&format!("{0:>width$}\n", letter, width = 2 * index));
+        } else {
+            result.push('\n');
+        }
+    }
+    result
+}
+
+fn lower_diamond(letter: char) -> String {
+    let letters = letters_upto(letter);
+    let half_width = letters.len();
+    let mut result = String::new();
+    for (index, letter) in letters.iter().rev().skip(1).enumerate() {
+        let left = format!("{0:>width$}", letter, width = index + 2);
+        result.push_str(&left);
+        if index != half_width - 2 {
+            result.push_str(&format!(
+                "{0:>width$}\n",
+                letter,
+                width = 2 * (half_width - index - 2)
+            ));
+        } else {
+            result.push('\n');
+        }
+    }
+    result
+}
+
+fn letters_upto(end: char) -> Vec<char> {
+    let upped = end.to_ascii_uppercase();
+    let end_index = UPPERS.find(upped);
+    match end_index {
+        Some(index) => UPPERS.chars().take(index + 1).collect(),
+        _ => UPPERS.chars().collect(),
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn letters_upto_singleton() {
+        assert_eq!(letters_upto('A'), vec!['A']);
     }
+
+    #[test]
+    fn letters_upto_c() {
+        assert_eq!(letters_upto('C'), vec!['A', 'B', 'C']);
+    }
+
+    #[test]
+    fn letters_upto_z() {
+        assert_eq!(letters_upto('Z'), UPPERS.chars().collect::<Vec<char>>());
+    }
+
+    #[test]
+    fn diamond_upper_two_rows() {
+        assert_eq!(
+            upper_diamond('B').trim(),
+            "
+ A
+B B
+"
+            .trim()
+        );
+    }
+
+    #[test]
+    fn diamond_upper_four_rows() {
+        assert_eq!(
+            upper_diamond('D').trim(),
+            "
+   A
+  B B
+ C   C
+D     D
+"
+            .trim()
+        );
+    }
+
+    #[test]
+    fn diamond_lower_four_rows() {
+        assert_eq!(
+            lower_diamond('D').trim(),
+            "
+ C   C
+  B B
+   A
+"
+            .trim()
+        );
+    }
+
+    #[test]
+    fn diamond_single_row() {
+        assert_eq!(diamond('A').trim(), "A");
+    }
+
+    #[test]
+    fn diamond_seven_rows() {
+        assert_eq!(
+            diamond('D').trim(),
+            "
+   A
+  B B
+ C   C
+D     D
+ C   C
+  B B
+   A
+"
+            .trim()
+        );
+    }
+
+    #[test]
+    fn diamond_nine_rows() {
+        assert_eq!(
+            diamond('E').trim(),
+            "
+    A
+   B B
+  C   C
+ D     D
+E       E
+ D     D
+  C   C
+   B B
+    A
+"
+            .trim()
+        );
+    }
+
 }
